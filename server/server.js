8c144e00656ecd8c7bf36cf9c9e981eecb10f471
Eric Thorfinnson <ethorf@gmail.com>
2020-03-27 11:10:35 -0400

    fluntilla

--- server/server.js
+++ server/server.js
@@ -5,10 +5,9 @@ const publicPath = path.join(__dirname, '..', 'portfolio/client/public');
 const PORT = process.env.PORT || 8080;
 
 if (process.env.NODE_ENV === 'production') {
-	// Set static folder
-	app.use(express.static('../client/build/index.html'));
+	app.use(express.static('client/build'));
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
 }
 app.listen(PORT, () => {

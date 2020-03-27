70bee2d5aa78c9646ebe952acc3e3b6ea2e0d7fb
Eric Thorfinnson <ethorf@gmail.com>
2020-03-27 10:41:45 -0400

    trying to troubleshoot deployment stuff

---
+++ server/build/precache-manifest.1b020a67fe597e96a792a5a58b68e482.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b1981b2e21b6de9d8f199857230aa61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a92acd3fa73b15697af",
+    "url": "/static/css/main.a168df85.chunk.css"
+  },
+  {
+    "revision": "c459b8508be26deb8b2f",
+    "url": "/static/js/2.cead6b2d.chunk.js"
+  },
+  {
+    "revision": "ce936309e3057cbb826ce85436799e62",
+    "url": "/static/js/2.cead6b2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a92acd3fa73b15697af",
+    "url": "/static/js/main.ff23d49d.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  },
+  {
+    "revision": "fc69b100c81352200b8d37d7e17c276f",
+    "url": "/static/media/Eric-Thorfinnson-Resume.fc69b100.pdf"
+  },
+  {
+    "revision": "8628eeb65f1bd338222e8a3f8fbc0674",
+    "url": "/static/media/EricFacePopup-cropped.8628eeb6.png"
+  },
+  {
+    "revision": "b04b0968e67dc05bb436fb6d689758e3",
+    "url": "/static/media/GitHub-light-silver.b04b0968.png"
+  },
+  {
+    "revision": "fe0ed590f2a4a6b71bc4dac2df9a711a",
+    "url": "/static/media/GitHub-silver.fe0ed590.png"
+  },
+  {
+    "revision": "d63f8cadc2fddf341abdd46cece41da1",
+    "url": "/static/media/Hosana-AlbumArt(small).d63f8cad.jpg"
+  },
+  {
+    "revision": "82d32b7ed3993add5e25d08fc5412390",
+    "url": "/static/media/NYT-preview-video-720-edited-2.82d32b7e.mp4"
+  },
+  {
+    "revision": "0dd5989a5fedaa61f738a9122c002919",
+    "url": "/static/media/amy-beth-anders-before-you-artwork.0dd5989a.jpg"
+  },
+  {
+    "revision": "2847656163f531c3134e39c00bbcbbe5",
+    "url": "/static/media/autocatalytica-eponymous-artwork.28476561.jpg"
+  },
+  {
+    "revision": "fbcc001d7dc91617cc466c50151010cd",
+    "url": "/static/media/autocatalytica-horror-vacui-artwork.fbcc001d.jpg"
+  },
+  {
+    "revision": "5b7c3d592c3422754193c16314275e16",
+    "url": "/static/media/autocatalytica-vicissitudes-artwork.5b7c3d59.jpg"
+  },
+  {
+    "revision": "811fddc70a3722e0f267e6395c99b652",
+    "url": "/static/media/bandcamp-light-silver.811fddc7.png"
+  },
+  {
+    "revision": "8a1ebd7ff5112acc861f87ee602947dd",
+    "url": "/static/media/bandcamp-silver.8a1ebd7f.png"
+  },
+  {
+    "revision": "854db7cb2bab1d1a42f1202b971fcef8",
+    "url": "/static/media/eric-thorfinnson-juan-artwork.854db7cb.jpg"
+  },
+  {
+    "revision": "654477479344555f5f9959c13a2ccbec",
+    "url": "/static/media/eric-thorfinnson-qwag-artwork.65447747.jpg"
+  },
+  {
+    "revision": "1cba47a664f158081484904862dd353c",
+    "url": "/static/media/eric-thorfinnson-solo-guitar-artwork.1cba47a6.jpg"
+  },
+  {
+    "revision": "14a8018e0485f2e8f8e99fb8612449ca",
+    "url": "/static/media/ex-kathedra-album-artwork.14a8018e.jpg"
+  },
+  {
+    "revision": "9a8c4171c999e249e3ab62fb7692793e",
+    "url": "/static/media/instagram-light-silver.9a8c4171.png"
+  },
+  {
+    "revision": "5a7016f89b182c0f24dd37863e4fb910",
+    "url": "/static/media/instagram-silver.5a7016f8.png"
+  },
+  {
+    "revision": "f1689b95c57847409a292534b7f51b05",
+    "url": "/static/media/linkedin-light-silver.f1689b95.png"
+  },
+  {
+    "revision": "9bb7dfcc8788a614ee9173c6c076de11",
+    "url": "/static/media/linkedin-silver.9bb7dfcc.png"
+  },
+  {
+    "revision": "622f9bfade5b72d710bf2be396730a36",
+    "url": "/static/media/not-your-thoughts-screencap.622f9bfa.png"
+  },
+  {
+    "revision": "5ee16f623f28f7fcd344a29f8b10e5e2",
+    "url": "/static/media/octoberguard.5ee16f62.ttf"
+  },
+  {
+    "revision": "d89b0c670d515dcd7b1918d56ec8f6b9",
+    "url": "/static/media/spotify-light-silver.d89b0c67.png"
+  },
+  {
+    "revision": "88ef55e41ef64b1870fa424a22ba92a6",
+    "url": "/static/media/spotify-silver.88ef55e4.png"
+  }
+]);
---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'portfolio/client/public');
+const PORT = process.env.PORT || 8080;
+
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}.`);
+});

5b1c7b89c9a83846b35e90a7c2b72e355b9ddf39
Eric Thorfinnson <ethorf@gmail.com>
2021-04-06 13:09:24 -0400

    updated with fortuna

--- client/src/components/webSection/webSection.js
+++ client/src/components/webSection/webSection.js
@@ -4,12 +4,90 @@ import NotYourThoughtsImage from '../../assets/not-your-thoughts-screencap.png';
 import GitHubDark from '../../assets/Icons/silver/GitHub-silver.png';
 import NYTVideo from '../../assets/NYT-preview-video-540.mp4';
 import feelScreenshot from '../../assets/feel-screenshot.png';
+import fortunaScreenshot from '../../assets/fortuna-video-screencap-no-ext.png';
 import bigDealScreenshot from '../../assets/bigdeal-screenshot.png';
 
 export default function WebSection(webSectionOpen) {
 	return (
 		<div className={`webSection ${webSectionOpen.webSectionOpen ? 'visible' : 'invisible'}`}>
+			<h1 className={`webSection__projects-header`}>Experience</h1>
+			<div className={`webSection__project-container`} data-aos="fade-in">
+				<a
+					rel="noopener noreferrer"
+					target="_blank"
+					href="http://video.fortuna.ai"
+					className={`webSection__fortuna-header`}
+				>
+					{' '}
+					Full-stack DevelopER - FoRtuna VidEo
+				</a>
+				<p className={`webSection__duration-subheader`}>Sept 2020-Apr 2021</p>
+				<a
+					rel="noopener noreferrer"
+					target="_blank"
+					href="http://video.fortuna.ai"
+					className={`webSection__fortuna-img-link-container`}
+				>
+					<img
+						className={`webSection__fortuna-image`}
+						src={fortunaScreenshot}
+						alt="fortuna preview "
+						data-aos="fade-in"
+					/>
+				</a>
+
+				<h3 className={`webSection__not-your-thoughts-description`} data-aos="fade-in">
+					<p>
+						Lead a team of developers and interns in the construction and maintenance of a MERN-stack web
+						app & Chrome extension-based video recording and delivery tool for financial advisors -
+					</p>
+					<p>
+						Responsible for the planning, execution, and deployment of a broad set of features including but
+						not limited to: video upload optimization, User Auth & Signup Flow, Payment processing via
+						Stripe, Video Trimming & Overlay functionality -
+					</p>
+					<p>Participated in code reviews and coordinated team-wide daily stand-up meetings - </p>
+					<p>Interfaced with clients to address concerns and troubleshoot bugs -</p>
+					<p>Spearheaded extension LinkedIn integration -</p>
+					<p>Established company-wide development best practices -</p>
+				</h3>
+			</div>
 			<h1 className={`webSection__projects-header`}>Projects</h1>
+			<div className={`webSection__project-container`} data-aos="fade-in">
+				<a
+					rel="noopener noreferrer"
+					target="_blank"
+					href="https://bigdeal-app.herokuapp.com/"
+					className={`webSection__not-your-thoughts-header webSection__feel-header  `}
+				>
+					{' '}
+					BigDeal
+				</a>
+				<img
+					className={`webSection__not-your-thoughts-image webSection__feel-image`}
+					src={bigDealScreenshot}
+					alt="bigDeal preview "
+					data-aos="fade-in"
+				/>
+
+				<h3 className={`webSection__not-your-thoughts-description`} data-aos="fade-in">
+					<p>
+						BigDeal is a web app developed as part of the Hatchways co-op program. Utilizing web-scraping
+						via Puppeteer.js, it allows users to create shopping lists and receive automated price updates
+						on products they are tracking. They are also able to follow other users and view their shopping
+						lists.{' '}
+					</p>
+					<p>Tech Stack: React + Node/Express + MongoDB </p>
+				</h3>
+				<a
+					rel="noopener noreferrer"
+					target="_blank"
+					href="https://github.com/hatchways/team-watermelon"
+					className={`social-buttons__button`}
+				>
+					<img src={GitHubDark} className={`webSection__button-img github`} />
+				</a>
+			</div>
 			<div className={`webSection__project-container`} data-aos="fade-in">
 				<a
 					rel="noopener noreferrer"
@@ -73,41 +151,6 @@ export default function WebSection(webSectionOpen) {
 					rel="noopener noreferrer"
 					target="_blank"
 					href="https://github.com/Ethorf/feel"
-					className={`social-buttons__button`}
-				>
-					<img src={GitHubDark} className={`webSection__button-img github`} />
-				</a>
-			</div>
-			<div className={`webSection__project-container`} data-aos="fade-in">
-				<a
-					rel="noopener noreferrer"
-					target="_blank"
-					href="https://bigdeal-app.herokuapp.com/"
-					className={`webSection__not-your-thoughts-header webSection__feel-header  `}
-				>
-					{' '}
-					BigDeal
-				</a>
-				<img
-					className={`webSection__not-your-thoughts-image webSection__feel-image`}
-					src={bigDealScreenshot}
-					alt="bigDeal preview "
-					data-aos="fade-in"
-				/>
-
-				<h3 className={`webSection__not-your-thoughts-description`} data-aos="fade-in">
-					<p>
-						BigDeal is a web app developed as part of the Hatchways co-op program. Utilizing web-scraping
-						via Puppeteer.js, it allows users to create shopping lists and receive automated price updates
-						on products they are tracking. They are also able to follow other users and view their shopping
-						lists.{' '}
-					</p>
-					<p>Tech Stack: React + Node/Express + MongoDB </p>
-				</h3>
-				<a
-					rel="noopener noreferrer"
-					target="_blank"
-					href="https://github.com/hatchways/team-watermelon"
 					className={`social-buttons__button`}
 				>
 					<img src={GitHubDark} className={`webSection__button-img github`} />
